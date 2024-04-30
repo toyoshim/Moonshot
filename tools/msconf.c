e1e7d8db0344efb35208d932c57f4783fde1a0a9
Takashi Toyoshima <toyoshim@gmail.com>
2024-05-01 06:05:29 +0900

    msconf.x 1.01.0

--- tools/msconf.c
+++ tools/msconf.c
@@ -5,11 +5,9 @@
 #include "mscmd.h"
 #include "mslib.h"
 
-// MOD BEGIN
 static const unsigned char msconf_version_major = 1;
-static const unsigned char msconf_version_minor = 0;
-static const unsigned char msconf_version_patch = 1;
-// MOD END
+static const unsigned char msconf_version_minor = 1;
+static const unsigned char msconf_version_patch = 0;
 
 static unsigned char version_major = 0;
 static unsigned char version_minor = 0;
@@ -28,15 +26,15 @@ enum {
   CURSOR_ANALOG_Y_MIN = 12,
   CURSOR_ANALOG_Y_MAX = 17,
   CURSOR_DIGITAL_X_MAX = 10,
-  CURSOR_ANALOG_X_MAX = 2,
+  CURSOR_ANALOG_X_MAX = 3,
 };
 
 void show_cursor(int active) {
   int digital_xs[11] = {
-    28, 32, 38, 42, 46, 52, 56, 60, 66, 70, 78,
+    26, 30, 36, 40, 44, 50, 54, 58, 64, 68, 78,
   };
-  int analog_xs[3] = {
-    76, 80, 84,
+  int analog_xs[4] = {
+    74, 78, 82, 86,
   };
   int widths[11] = {
     2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  5,
@@ -72,8 +70,8 @@ void show_rapid_fire(int y, int x, int mode) {
 }
 
 void show_version() {
-  printf("\033[%d;%dH%d.%02d.%d", 27, 68, msconf_version_major, msconf_version_minor, msconf_version_patch);
-  printf("\033[%d;%dH%d.%02d.%d", 28, 68, version_major, version_minor, version_patch);
+  printf("\033[%d;%dH%d.%02d.%d", 27, 66, msconf_version_major, msconf_version_minor, msconf_version_patch);
+  printf("\033[%d;%dH%d.%02d.%d", 28, 66, version_major, version_minor, version_patch);
 }
 
 void show_status(const char* status) {
@@ -83,26 +81,27 @@ void show_status(const char* status) {
 }
 
 void show_digital(int i) {
-  show_button(7 + i, 29, config.digital[i].map1 & 0x40);  /* Select */
-  show_button(7 + i, 33, config.digital[i].map1 & 0x80);  /* Start  */
-  show_button(7 + i, 39, config.digital[i].map1 & 0x02);  /* 1      */
-  show_button(7 + i, 43, config.digital[i].map1 & 0x01);  /* 2      */
-  show_button(7 + i, 47, config.digital[i].map2 & 0x80);  /* 3      */
-  show_button(7 + i, 53, config.digital[i].map2 & 0x40);  /* 4      */
-  show_button(7 + i, 57, config.digital[i].map2 & 0x20);  /* 5      */
-  show_button(7 + i, 61, config.digital[i].map2 & 0x10);  /* 6      */
-  show_button(7 + i, 67, config.digital[i].map2 & 0x08);  /* 7      */
-  show_button(7 + i, 71, config.digital[i].map2 & 0x04);  /* 8      */
+  show_button(7 + i, 27, config.digital[i].map1 & 0x40);  /* Select */
+  show_button(7 + i, 31, config.digital[i].map1 & 0x80);  /* Start  */
+  show_button(7 + i, 37, config.digital[i].map1 & 0x02);  /* 1      */
+  show_button(7 + i, 41, config.digital[i].map1 & 0x01);  /* 2      */
+  show_button(7 + i, 45, config.digital[i].map2 & 0x80);  /* 3      */
+  show_button(7 + i, 51, config.digital[i].map2 & 0x40);  /* 4      */
+  show_button(7 + i, 55, config.digital[i].map2 & 0x20);  /* 5      */
+  show_button(7 + i, 59, config.digital[i].map2 & 0x10);  /* 6      */
+  show_button(7 + i, 65, config.digital[i].map2 & 0x08);  /* 7      */
+  show_button(7 + i, 69, config.digital[i].map2 & 0x04);  /* 8      */
 
   show_rapid_fire(7 + i, 79, config.digital[i].rapid_fire);
 }
 
 void show_analog(int i) {
   unsigned char target = config.analog[i];
-  printf("\033[%d;%dH%s  %s  %s", 23 + i, 77,
+  printf("\033[%d;%dH%s  %s  %s  %s", 23 + i, 75,
 	       (target == 0) ? "œ" : "›",
 	       (target == 1) ? "œ" : "›",
-	       (target == 2) ? "œ" : "›");
+	       (target == 2) ? "œ" : "›",
+	       (target == 3) ? "œ" : "›");
 }
 
 void show_config() {
@@ -172,76 +171,77 @@ void update() {
     }
   }
   if (last_data[0] != data[0]) {
-    show_button( 8, 47, ~data[0] & 0x20);  /* C    */
-    show_button( 8, 53, ~data[0] & 0x10);  /* D    */
-    show_button( 8, 57, ~data[0] & 0x08);  /* E1   */
-    show_button( 8, 61, ~data[0] & 0x04);  /* E2   */
-    show_button( 8, 33, ~data[0] & 0x02);  /* St   */
-    show_button( 8, 29, ~data[0] & 0x01);  /* Sl   */
+    show_button( 8, 45, ~data[0] & 0x20);  /* C    */
+    show_button( 8, 51, ~data[0] & 0x10);  /* D    */
+    show_button( 8, 55, ~data[0] & 0x08);  /* E1   */
+    show_button( 8, 59, ~data[0] & 0x04);  /* E2   */
+    show_button( 8, 31, ~data[0] & 0x02);  /* St   */
+    show_button( 8, 27, ~data[0] & 0x01);  /* Sl   */
     last_data[0] = data[0];
   }
   if (last_data[1] != data[1] || last_data[3] != data[3]) {
-    show_value( 8, 77, (data[1] & 0xf0) | (data[3] >> 4));
-    show_value( 8, 81, (data[1] << 4) | (data[3] & 0x0f));
+    show_value( 8, 75, (data[1] & 0xf0) | (data[3] >> 4));
+    show_value( 8, 79, (data[1] << 4) | (data[3] & 0x0f));
     last_data[1] = data[1];
     last_data[3] = data[3];
   }
   if (last_data[2] != data[2] || last_data[4] != data[4]) {
-    show_value( 8, 85, (data[2] & 0xf0) | (data[4] >> 4));
+    show_value( 8, 83, (data[2] & 0xf0) | (data[4] >> 4));
+    show_value( 8, 87, (data[2] << 4) | (data[4] & 0x0f));
     last_data[2] = data[2];
     last_data[4] = data[4];
   }
   if (last_data[5] != data[5]) {
-    show_button( 8, 39, ~data[5] & 0x80);  /* A    */
-    show_button( 8, 43, ~data[5] & 0x40);  /* B    */
-    show_button( 8, 67, ~data[5] & 0x20);  /* A'   */
-    show_button( 8, 71, ~data[5] & 0x10);  /* B'   */
+    show_button( 8, 37, ~data[5] & 0x80);  /* A    */
+    show_button( 8, 41, ~data[5] & 0x40);  /* B    */
+    show_button( 8, 65, ~data[5] & 0x20);  /* A'   */
+    show_button( 8, 69, ~data[5] & 0x10);  /* B'   */
     last_data[5] = data[5];
   }
   if (last_data[6] != data[6]) {
-    show_button( 6, 13, data[ 6] & 0x80);  /* UP    */
-    show_button( 8, 13, data[ 6] & 0x40);  /* DOWN  */
-    show_button( 7, 11, data[ 6] & 0x20);  /* LEFT  */
-    show_button( 7, 15, data[ 6] & 0x10);  /* RIGHT */
-    show_button(11, 23, data[ 6] & 0x08);  /* Dig 1 */
-    show_button(12, 23, data[ 6] & 0x04);  /* Dig 2 */
-    show_button(13, 23, data[ 6] & 0x02);  /* Dig 3 */
-    show_button(14, 23, data[ 6] & 0x01);  /* Dig 4 */
+    show_button( 6, 11, data[ 6] & 0x80);  /* UP    */
+    show_button( 8, 11, data[ 6] & 0x40);  /* DOWN  */
+    show_button( 7,  9, data[ 6] & 0x20);  /* LEFT  */
+    show_button( 7, 13, data[ 6] & 0x10);  /* RIGHT */
+    show_button(11, 21, data[ 6] & 0x08);  /* Dig 1 */
+    show_button(12, 21, data[ 6] & 0x04);  /* Dig 2 */
+    show_button(13, 21, data[ 6] & 0x02);  /* Dig 3 */
+    show_button(14, 21, data[ 6] & 0x01);  /* Dig 4 */
     last_data[6] = data[6];
   }
   if (last_data[7] != data[7]) {
-    show_button(15, 23, data[ 7] & 0x80);  /* Dig 5 */
-    show_button(16, 23, data[ 7] & 0x40);  /* Dig 6 */
-    show_button(17, 23, data[ 7] & 0x20);  /* Dig 7 */
-    show_button(18, 23, data[ 7] & 0x10);  /* Dig 8 */
-    show_button(19, 23, data[ 7] & 0x08);  /* Dig 9 */
-    show_button(20, 23, data[ 7] & 0x04);  /* Dig A */
-    show_button(21, 23, data[ 7] & 0x02);  /* Dig B */
-    show_button(22, 23, data[ 7] & 0x01);  /* Dig C */
+    show_button(15, 21, data[ 7] & 0x80);  /* Dig 5 */
+    show_button(16, 21, data[ 7] & 0x40);  /* Dig 6 */
+    show_button(17, 21, data[ 7] & 0x20);  /* Dig 7 */
+    show_button(18, 21, data[ 7] & 0x10);  /* Dig 8 */
+    show_button(19, 21, data[ 7] & 0x08);  /* Dig 9 */
+    show_button(20, 21, data[ 7] & 0x04);  /* Dig A */
+    show_button(21, 21, data[ 7] & 0x02);  /* Dig B */
+    show_button(22, 21, data[ 7] & 0x01);  /* Dig C */
     last_data[7] = data[7];
   }
   if (last_data[10] != data[10]) {
-    show_value(23, 23, data[10]);          /* Ana 1 */
+    show_value(23, 21, data[10]);          /* Ana 1 */
     last_data[10] = data[10];
   }
   if (last_data[11] != data[11]) {
-    show_value(24, 23, data[11]);          /* Ana 2 */
+    show_value(24, 21, data[11]);          /* Ana 2 */
     last_data[11] = data[11];
   }
   if (last_data[12] != data[12]) {
-    show_value(25, 23, data[12]);          /* Ana 3 */
+    show_value(25, 21, data[12]);          /* Ana 3 */
     last_data[12] = data[12];
   }
   if (last_data[14] != data[14]) {
-    show_value(26, 23, data[14]);          /* Ana 4 */
+    show_value(26, 21, data[14]);          /* Ana 4 */
     last_data[14] = data[14];
   }
   if (last_data[15] != data[15]) {
-    show_value(27, 23, data[15]);          /* Ana 5 */
+    show_value(27, 21, data[15]);          /* Ana 5 */
     last_data[15] = data[15];
   }
   if (last_data[16] != data[16]) {
-    show_value(28, 23, data[16]);          /* Ana 6 */
+    show_value(28, 21, data[16]);          /* Ana 6 */
     last_data[16] = data[16];
   }
 }
@@ -249,51 +249,49 @@ void update() {
 void screen_setup() {
   char* screen[] = {
     "",
-    "                       * Moonshot Layout Confugurations *",
-    "„¬„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„­",
-    "„« [OUTPUT]  CPSF „  Sl  St „  ‚`  ‚a  ‚k „  ‚w  ‚x  ‚q                         „«",
-    "„°„ª„ª„ª„ª„­ MD6B „  Sl  St „  ‚`  ‚a  ‚b „  ‚w  ‚x  ‚y                         „«",
-    "„«   ›   „« Cyber„  Sl  St „  ‚`  ‚a  ‚b „  ‚c  E1  E2 „  ‚`' ‚a'„  ‚x  ‚w  Th „«",
-    "„« ›@› „¯„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„²",
-    "„«   ›           „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „ $00 $00 $00 „«",
-    "„°„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„²",
-    "„« [INPUT]        „               Layout Map Matrix               „  RAPID FIRE „«",
-    "„« Digital 1   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „     OFF     „«",
-    "„« Digital 2   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „     OFF     „«",
-    "„« Digital 3   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „     OFF     „«",
-    "„« Digital 4   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „     OFF     „«",
-    "„« Digital 5   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „     OFF     „«",
-    "„« Digital 6   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „     OFF     „«",
-    "„« Digital 7   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „     OFF     „«",
-    "„« Digital 8   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „     OFF     „«",
-    "„« Digital 9   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „     OFF     „«",
-    "„« Digital A   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „     OFF     „«",
-    "„« Digital B   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „     OFF     „«",
-    "„« Digital C   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „     OFF     „«",
-    "„« Analog  1  $00 „                                                  ›  ›  › „«",
-    "„« Analog  2  $00 „   S: Save to Moonshot                            ›  ›  › „«",
-    "„« Analog  3  $00 „   Q: Quit                                        ›  ›  › „«",
-    "„« Analog  4  $00 „                                                  ›  ›  › „«",
-    "„« Analog  5  $00 „                      MsConfig Version : 0.00.0   ›  ›  › „«",
-    "„« Analog  6  $00 „                      Firmware Version : 0.00.0   ›  ›  › „«",
-    "„¯„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„®",
+    "                          * Moonshot Layout Confugurations *                          ",
+    "„¬„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„­",
+    "„« [OUTPUT]  CPSF „  Sl  St „  ‚`  ‚a  ‚k „  ‚w  ‚x  ‚q                             „«",
+    "„°„ª„ª„ª„ª„­ MD6B „  Sl  St „  ‚`  ‚a  ‚b „  ‚w  ‚x  ‚y                             „«",
+    "„«   ›   „« Cyber„  Sl  St „  ‚`  ‚a  ‚b „  ‚c  E1  E2 „  ‚`' ‚a'„  ‚x  ‚w  Th  X2 „«",
+    "„« ›@› „¯„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„²",
+    "„«   ›           „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „ $00 $00 $00 $00 „«",
+    "„°„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„²",
+    "„« [INPUT]        „               Layout Map Matrix               „    RAPID FIRE   „«",
+    "„« Digital 1   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „       OFF       „«",
+    "„« Digital 2   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „       OFF       „«",
+    "„« Digital 3   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „       OFF       „«",
+    "„« Digital 4   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „       OFF       „«",
+    "„« Digital 5   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „       OFF       „«",
+    "„« Digital 6   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „       OFF       „«",
+    "„« Digital 7   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „       OFF       „«",
+    "„« Digital 8   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „       OFF       „«",
+    "„« Digital 9   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „       OFF       „«",
+    "„« Digital A   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „       OFF       „«",
+    "„« Digital B   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „       OFF       „«",
+    "„« Digital C   › „  ›  › „  ›  ›  › „  ›  ›  › „  ›  › „       OFF       „«",
+    "„« Analog  1  $00 „                                                  ›  ›  ›  › „«",
+    "„« Analog  2  $00 „   S: Save to Moonshot                            ›  ›  ›  › „«",
+    "„« Analog  3  $00 „   Q: Quit                                        ›  ›  ›  › „«",
+    "„« Analog  4  $00 „                                                  ›  ›  ›  › „«",
+    "„« Analog  5  $00 „                      MsConfig Version : 0.00.0   ›  ›  ›  › „«",
+    "„« Analog  6  $00 „                      Firmware Version : 0.00.0   ›  ›  ›  › „«",
+    "„¯„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„ª„®",
     0,
   };
   int i;
   printf("\033[2J");
   for (i = 0; screen[i]; ++i) {
-    printf("\033[%dC%s\n", 7, screen[i]);
+    printf("\033[%dC%s\n", 5, screen[i]);
   }
   fflush(stdout);
 }
 
 int setup(void) {
   int result = ms_get_version(&version_major, &version_minor, &version_patch);
-// MOD BEGIN
   if (result == 0) {
     slow_mode = 0;
   } else {
-// MOD END
     fprintf(stderr, "Moonshot (%d): device not found, retrying with slow mode option\n",
 	    result);
     ms_enter_slow_mode();
@@ -307,6 +305,7 @@ int setup(void) {
   result = ms_load_config(&config);
   if (result != 0) {
     fprintf(stderr, "Moonshot (%d): communication failed to load configuration\n", result);
+    fprintf(stderr, "firmware: %d.%02d.%d\n", version_major, version_minor, version_patch);
     return 2;
   }
 
